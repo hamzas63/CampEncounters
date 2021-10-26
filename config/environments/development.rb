6cbe6036977ad81bf7ffdb625264f75cc85aa5e5
Hamza Arshad <hamza.arshad@square63.org>
2021-10-26 18:31:47 +0500

    Added device integrated gem: -Email confirmation before login -Flash messages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = {address: 'localhost', port: 1025}
+  config.action_mailer.delivery_method = :smtp
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 

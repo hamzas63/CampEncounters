e18465a09e1a340f089b46178fb51839d2c38d1e
Hamza Arshad <hamza.arshad@square63.org>
2021-10-27 16:58:33 +0500

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2021_10_26_103908) do
+ActiveRecord::Schema.define(version: 2021_10_27_065139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_103908) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.integer "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

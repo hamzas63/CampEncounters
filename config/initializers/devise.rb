0c048a96ab29e0b2a4b124b0043a312b227b1779
Hamza Arshad <hamza.arshad@square63.org>
2021-10-27 11:18:50 +0500

    Features added: 1-Login Page 2-Forgot Password 3-Email confirmation for forgot password

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+Devise::TRUE_VALUES << ["on"]
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -163,7 +163,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  # config.remember_for = 2.weeks
+  config.remember_for = 2.weeks
 
   # Invalidates all the remember me tokens when the user signs out.
   config.expire_all_remember_me_on_sign_out = true

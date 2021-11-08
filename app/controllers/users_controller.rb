aeb44070563380212504d1822baa4dc8b42d1f3c
Hamza Arshad <hamza.arshad@square63.org>
2021-11-08 11:07:06 +0500

    Changed syntax of csv function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,13 +54,13 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = current_user
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = current_user
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :role, :middle_name, :phone, :country, :type, :image)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :role, :middle_name, :phone, :country, :type, :image)
+  end
 end
